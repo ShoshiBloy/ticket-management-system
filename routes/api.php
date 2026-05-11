faae2c9d303bb13cd44257947f63dc7e45ae3ba1
ShoshiBloy <hand1c5@mbjcomp.org.il>
2026-05-12 00:21:26 +0300

    Enable API routes and register ticket endpoints

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TicketController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('tickets',TicketController::class);
+
+Route::patch('tickets/{ticket}/status',[TicketController::class,'changeStatus']);
+Route::patch('tickets/{ticket}/assign',[TicketController::class,'assign']);
